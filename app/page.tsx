6b48c74c317a9941fd06e25a0d913963754e6e23
skowalskidev <skowalskidev@gmail.com>
2023-07-06 13:04:47 +0700

    first value offering

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,132 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [estimate, setEstimate] = useState({
+    developerHours: 0,
+    hourlyRate: 0,
+    designerHours: 0,
+    designerHourlyRate: 0,
+    developmentCost: 0,
+    designerCost: 0,
+    hostingCost: 0,
+    cdnCost: 0,
+    ourFee: 0,
+    totalEstimate: 0,
+  });
+
+  // Function to calculate the estimate using AI
+  const calculateEstimate = () => {
+    // Replace this placeholder logic with your actual AI estimation implementation
+    const developerHours = 40; // Placeholder value
+    const hourlyRate = 50; // Placeholder value
+    const designerHours = 20; // Placeholder value
+    const designerHourlyRate = 60; // Placeholder value
+    const developmentCost = developerHours * hourlyRate;
+    const designerCost = designerHours * designerHourlyRate;
+    const hostingCost = 100; // Placeholder value
+    const cdnCost = 50; // Placeholder value
+    const ourFee = (developmentCost + designerCost + hostingCost + cdnCost) * 0.04;
+    const totalEstimate = developmentCost + designerCost + hostingCost + cdnCost + ourFee;
+
+    setEstimate({
+      developerHours,
+      hourlyRate,
+      designerHours,
+      designerHourlyRate,
+      developmentCost,
+      designerCost,
+      hostingCost,
+      cdnCost,
+      ourFee,
+      totalEstimate,
+    });
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <h1 className="text-4xl font-bold text-center">
+        100% transparent web development
+      </h1>
+
+      <section className="my-12">
+        <h2 className="text-2xl font-bold">Get an AI Estimate</h2>
+        <p>Fill out the form below to get an instant AI estimate:</p>
+        <form>
+          {/* Include relevant form fields for project details */}
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={calculateEstimate}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Get Estimate
+          </button>
+        </form>
+        {estimate.developerHours > 0 && estimate.hourlyRate > 0 && (
+          <div>
+            <p>
+              Developer Hours: {estimate.developerHours} hours
+              <br />
+              Developer Hourly Rate: ${estimate.hourlyRate}
+              <br />
+              Designer Hours: {estimate.designerHours} hours
+              <br />
+              Designer Hourly Rate: ${estimate.designerHourlyRate}
+              <br />
+              Development Cost: ${estimate.developmentCost}
+              <br />
+              Designer Cost: ${estimate.designerCost}
+              <br />
+              Hosting Cost: ${estimate.hostingCost}
+              <br />
+              CDN Cost: ${estimate.cdnCost}
+              <br />
+              Our Fee (4%): ${estimate.ourFee}
+              <br />
+              Total Estimate: ${estimate.totalEstimate}
+            </p>
+          </div>
+        )}
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="my-12">
+        <h2 className="text-2xl font-bold">How It Works</h2>
+        <ol className="list-decimal list-inside">
+          <li>Specify your requirements and project details</li>
+          <li>Approve the design proposal provided by our team</li>
+          <li>Development commences based on the approved design</li>
+        </ol>
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="my-12">
+        <h2 className="text-2xl font-bold">Why Choose Us</h2>
+        <ul className="list-disc list-inside">
+          <li>No time wasted on lengthy phone calls</li>
+          <li>Efficient communication via messages to streamline operations</li>
+          <li>Reduced overhead costs allow us to pass on savings to our clients</li>
+          <li>Expertise and industry knowledge applied to deliver exceptional results</li>
+        </ul>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className="my-12">
+        <h2 className="text-2xl font-bold">For Clients</h2>
+        <ul className="list-disc list-inside">
+          <li>Create high-quality websites that leave a lasting impression</li>
+          <li>Benefit from our transparent rates with no hidden fees</li>
+          <li>Get fair and competitive pricing, ensuring you receive the best value</li>
+        </ul>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="my-12">
+        <h2 className="text-2xl font-bold">For Developers and Designers</h2>
+        <ul className="list-disc list-inside">
+          <li>Join us for opportunities to work on top-notch projects</li>
+          <li>Fulfill your passion for delivering high-quality websites</li>
+          <li>Enjoy transparent rates and fair compensation for your expertise</li>
+          <li>Experience efficient communication and reduced overhead</li>
+        </ul>
+      </section>
     </main>
-  )
+  );
 }
