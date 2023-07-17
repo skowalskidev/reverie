26408a0e88a7386d14aab654edad0c9a55494a99
skowalskidev <skowalskidev@gmail.com>
2023-07-17 13:01:29 +0700

    add images to the guide

--- app/page.tsx
+++ app/page.tsx
@@ -918,30 +918,46 @@ export default function Home() {
                     <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                         <div className="mb-8 lg:mb-16">
                             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center">Free guide</h2>
-                            <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">How do I get a good quality website for a market price</p>
+                            <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">How do I get a good quality website for a market price?</p>
                         </div>
                         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/money.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of money"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">How do I not get ripped off / overcharged?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">How do I not get ripped off / overcharged?</h3>
                                 <p className="text-gray-500 dark:text-gray-400">Find out exactly how much you're paying for each development stage and compare that to the market rates. We haven't seen that yet, that's why we launched Reverie.</p>
-                                <h4 className='text-gray-500 dark:text-gray-400 font-bold mt-2'>Ans: Use us</h4>
                             </div>
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/podium.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of a podium"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">What’s the best web development agency?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">What’s the best web development agency?</h3>
                                 <p className="text-gray-500 dark:text-gray-400">They all say they're the best though, including us. Go figure. So to make the choice easier we'll make sure you get a quality website for a market price.</p>
-                                <p className='text-gray-500 dark:text-gray-400 font-bold mt-2'>Ans: None</p>
                             </div>
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/technology.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of money"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">What’s the best technology to build my business website?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">What’s the best technology to build my business website?</h3>
                                 <h4 className='text-gray-500 dark:text-gray-400 font-bold'>Good website ranking in Google</h4>
                                 <ul className='text-gray-500 dark:text-gray-400 list-disc'>
                                     <li>
@@ -964,37 +980,57 @@ export default function Home() {
                                     </li>
                                 </ul>
                             </div>
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/dollar.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of money"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">What’s the cheapest way to build a website?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">What’s the cheapest way to build a website?</h3>
                                 <p className="text-gray-500 dark:text-gray-400">You can do it yourself using a website builder or Wordpress but consider the quality of the website you're able to produce and the impression you'll make on your customers with a one size fits all design.</p>
-                                <p className='text-gray-500 dark:text-gray-400 font-bold mt-2'>Ans: Use a professional service</p>
                             </div>
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/offers.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of money"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">How to pick a web development agency?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">How to pick a web development agency?</h3>
                                 <p className="text-gray-500 dark:text-gray-400">Get quotes from at least 3 web development agencies, check the technology they're going to use. Or we can do all of that for you and tell you exactly how much we charge for this service.</p>
-                                <p className='text-gray-500 dark:text-gray-400 font-bold mt-2'>Ans: Vet professionals or let us vet for you</p>
                             </div>
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/money.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of money"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">Should I get a Wordpress website in 2023?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">Should I get a Wordpress website in 2023?</h3>
                                 <p className="text-gray-500 dark:text-gray-400">No, you really shouldn't unless you just want an online business card and spend a lot of time and money maintaining it.</p>
-                                <p className='text-gray-500 dark:text-gray-400 font-bold mt-2'>Ans: No</p>
                             </div>
-                            <div>
-                                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd"></path></svg>
+                            <div className='flex flex-col items-stretch gap-2'>
+                                <div className='relative h-44'>
+                                    <Image
+                                        src="/images/code.png"
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        fill
+                                        alt="Picture of money"
+                                        className='rounded object-cover'
+                                    />
                                 </div>
-                                <h3 className="mb-2 text-xl font-bold dark:text-white">I'm not worried about code quality, nobody will see it anyway, right?</h3>
+                                <h3 className="text-xl font-bold dark:text-white">I'm not worried about code quality, nobody will see it anyway, right?</h3>
                                 <p className="text-gray-500 dark:text-gray-400">You're kind of right, neither you nor your customer will but Google will notice milliseconds of slower loading times, pushing your website down in search results, so <span className='font-bold'>less clients</span> for you. When you need to maintain your website it will take developers longer meaning they will <span className='font-bold'>charge you more</span>. Finally, Wordpress is the no.1 target for hackers. In your first week after lauch your website admin panel will receive hundreds of login attempts from strangers as well as comments from scammers asking you to click on dodgy website links, <span className='font-bold'>costing you time</span> to protect against.</p>
-                                <p className='text-gray-500 dark:text-gray-400 font-bold mt-2'>Ans: It will cost you time, money and clients</p>
                             </div>
                         </div>
                     </div>
