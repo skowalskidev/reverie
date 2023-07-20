4939843df33613914ddd0287356ab71f0dcc2b07
skowalskidev <skowalskidev@gmail.com>
2023-07-20 11:47:05 +0700

    new logo text style

--- app/page.tsx
+++ app/page.tsx
@@ -103,10 +103,8 @@ export default function Home() {
         <>
             <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 ">
                 <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto px-4 py-2 relative">
-                    <a href="#" className="flex items-top">
-                        <p className="text-lg font-light text-gray-500/100 dark:text-gray-400/100">Reverie</p>
-                        <p className="hidden sm:block ml-2 text-lg font-light text-gray-500/50 dark:text-gray-400/50">Reverie</p>
-                        <p className="hidden sm:block ml-2 text-lg font-light text-gray-500/25 dark:text-gray-400/25">Reverie</p>
+                    <a href="#" className="flex items-top text-sm font-light text-gray-500/100 dark:text-gray-400/100 tracking-wider">
+                        <p className="text-sm tracking-widest font-light text-gray-500/100 text-gray-900 dark:text-white">Reverie</p>
                     </a>
                     <div className="flex md:order-2 gap-2 items-center">
                         <StickyMenuCTA heroSectionHeight={heroSectionHeight} />
@@ -147,7 +145,7 @@ export default function Home() {
                             </svg>
                         </button>
                     </div>
-                    <div className="items-center justify-center hidden w-full md:flex md:w-auto md:order-1 absolute left-0 right-0" id="navbar-sticky">
+                    <div className="items-center justify-center hidden w-fit md:flex md:order-1 absolute mx-auto left-0 right-0" id="navbar-sticky">
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <a href="#free-guide" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-600 md:p-0 md:dark:hover:text-purple-600 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Free guide</a>
@@ -156,36 +154,6 @@ export default function Home() {
                     </div>
                 </div>
             </nav>
-
-            <button
-                id="theme-toggle"
-                type="button"
-                className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
-                onClick={toggleTheme}
-            >
-                <svg
-                    id="theme-toggle-dark-icon"
-                    className={`${!isDarkMode ? 'w-5 h-5' : 'hidden w-5 h-5'}`}
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                </svg>
-                <svg
-                    id="theme-toggle-light-icon"
-                    className={`${isDarkMode ? 'w-5 h-5' : 'hidden w-5 h-5'}`}
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                    ></path>
-                </svg>
-            </button>
 
             <main className='mx-auto max-w-screen-lg'>
                 <section id='hero-section' className="bg-white dark:bg-gray-900 mt-28 flex items-end">
