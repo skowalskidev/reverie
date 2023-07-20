7bcc14143dacba75e3e3cd033b2e8144f6d189ac
skowalskidev <skowalskidev@gmail.com>
2023-07-20 12:32:29 +0700

    add icons to free guide

--- app/page.tsx
+++ app/page.tsx
@@ -141,7 +141,7 @@ export default function Home() {
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
                     </div>
@@ -403,40 +403,22 @@ export default function Home() {
                             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center">Free guide</h2>
                             <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center">How do I get a good quality website for a market price?</p>
                         </div>
-                        <div className="space-y-8 md:grid md:grid-cols-[176px,auto,auto] md:gap-12 md:space-y-0">
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/money.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of money"
-                                    className='rounded object-cover'
-                                />
-                            </div>
+                        <div className="space-y-8 md:grid md:grid-cols-[auto,auto,auto] md:gap-12 md:space-y-0">
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1M2 5h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Zm8 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
+                            </svg>
                             <h3 className="text-xl font-bold dark:text-white">How do I not get ripped off / overcharged?</h3>
                             <p className="text-gray-500 dark:text-gray-400">Find out exactly how much you're paying for each development stage and compare that to the market rates. We haven't seen that yet, that's why we launched Reverie.</p>
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/podium.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of a podium"
-                                    className='rounded object-cover'
-                                />
-                            </div>
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="m6.072 10.072 2 2 6-4m3.586 4.314.9-.9a2 2 0 0 0 0-2.828l-.9-.9a2 2 0 0 1-.586-1.414V5.072a2 2 0 0 0-2-2H13.8a2 2 0 0 1-1.414-.586l-.9-.9a2 2 0 0 0-2.828 0l-.9.9a2 2 0 0 1-1.414.586H5.072a2 2 0 0 0-2 2v1.272a2 2 0 0 1-.586 1.414l-.9.9a2 2 0 0 0 0 2.828l.9.9a2 2 0 0 1 .586 1.414v1.272a2 2 0 0 0 2 2h1.272a2 2 0 0 1 1.414.586l.9.9a2 2 0 0 0 2.828 0l.9-.9a2 2 0 0 1 1.414-.586h1.272a2 2 0 0 0 2-2V13.8a2 2 0 0 1 .586-1.414Z" />
+                            </svg>
                             <h3 className="text-xl font-bold dark:text-white">What’s the best web development agency?</h3>
-                            <p className="text-gray-500 dark:text-gray-400">They all say they're the best though, including us. Go figure. So to make the choice easier we'll make sure you get a quality website for a market price.</p>
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/technology.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of money"
-                                    className='rounded object-cover'
-                                />
-                            </div>
+                            <p className="text-gray-500 dark:text-gray-400">They all say they're the best though, including us. Go figure. So to make the choice easier we'll be 100% transparent with you.</p>
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="none" viewBox="0 0 20 18">
+                                <path stroke="currentColor" strokeLinecap="round" strokeWidth="1" d="M1 7h18M4 4h.01M7 4h.01M10 4h.01M3 17h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H3a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2Z" />
+                            </svg>
                             <h3 className="text-xl font-bold dark:text-white">What’s the best technology to build my business website?</h3>
-                            <div><h4 className='text-gray-500 dark:text-gray-400 font-bold'>Good website ranking in Google</h4>
+                            <div><h4 className='text-gray-500 dark:text-gray-400 font-bold'>Fast website ranking in Google</h4>
                                 <ul className='text-gray-500 dark:text-gray-400 list-disc'>
                                     <li>
                                         Next.js
@@ -447,61 +429,29 @@ export default function Home() {
                                     <li>
                                         Vercel hosting + Content Delivery Network
                                     </li>
-                                    <li>
-                                        Fear not, it's super easy to use after we set it up for you.
-                                    </li>
                                 </ul>
-                                <h4 className='text-gray-500 dark:text-gray-400 font-bold'>Simple business card site</h4>
-                                <ul className='text-gray-500 dark:text-gray-400 list-disc'>
-                                    <li>
-                                        Use a website builder or Wordpress.
-                                    </li>
-                                </ul>
+                                <p className='text-gray-500 dark:text-gray-400 mt-3'>Fear not, it's super easy to manage after we set it up for you.</p>
                             </div>
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/dollar.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of money"
-                                    className='rounded object-cover'
-                                />
-                            </div>
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 11 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M1.75 15.363a4.954 4.954 0 0 0 2.638 1.574c2.345.572 4.653-.434 5.155-2.247.502-1.813-1.313-3.79-3.657-4.364-2.344-.574-4.16-2.551-3.658-4.364.502-1.813 2.81-2.818 5.155-2.246A4.97 4.97 0 0 1 10 5.264M6 17.097v1.82m0-17.5v2.138" />
+                            </svg>
                             <h3 className="text-xl font-bold dark:text-white">What’s the cheapest way to build a website?</h3>
                             <p className="text-gray-500 dark:text-gray-400">You can do it yourself using a website builder or Wordpress but consider the quality of the website you're able to produce and the impression you'll make on your customers with a one size fits all design.</p>
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/offers.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of money"
-                                    className='rounded object-cover'
-                                />
-                            </div>
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1" />
+                            </svg>
                             <h3 className="text-xl font-bold dark:text-white">How to pick a web development agency?</h3>
                             <p className="text-gray-500 dark:text-gray-400">Get quotes from at least 3 web development agencies, check the technology they're going to use. Or we can do all of that for you and tell you exactly how much we charge for this service.</p>
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/money.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of money"
-                                    className='rounded object-cover'
-                                />
-                            </div>
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M6 1v4a1 1 0 0 1-1 1H1m4 4 1 6 2-4 2 4 1-6m4-8v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z" />
+                            </svg>
                             <h3 className="text-xl font-bold dark:text-white">Should I get a Wordpress website in 2023?</h3>
                             <p className="text-gray-500 dark:text-gray-400">No, you really shouldn't unless you just want an online business card and spend a lot of time and money maintaining it.</p>
-                            <div className='relative w-44 h-44'>
-                                <Image
-                                    src="/images/code.png"
-                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    fill
-                                    alt="Picture of money"
-                                    className='rounded object-cover'
-                                />
-                            </div>
-                            <h3 className="text-xl font-bold dark:text-white">I'm not worried about code quality, nobody will see it anyway, right?</h3>
-                            <p className="text-gray-500 dark:text-gray-400">You're kind of right, neither you nor your customer will but Google will notice milliseconds of slower loading times, pushing your website down in search results, so <span className='font-bold'>less clients</span> for you. When you need to maintain your website it will take developers longer meaning they will <span className='font-bold'>charge you more</span>. Finally, Wordpress is the no.1 target for hackers. In your first week after lauch your website admin panel will receive hundreds of login attempts from strangers as well as comments from scammers asking you to click on dodgy website links, <span className='font-bold'>costing you time</span> to protect against.</p>
+                            <svg className="w-10 h-10 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15" />
+                            </svg>
+                            <h3 className="text-xl font-bold dark:text-white">Code quality, nobody will see it anyway, right?</h3>
+                            <p className="text-gray-500 dark:text-gray-400">You're kind of right, neither you nor your customer will but Google will notice milliseconds of slower loading times, pushing your website down in search results, so <span className='font-bold'>less clients</span> will find you. When you need to maintain your website it will take developers longer meaning they will <span className='font-bold'>charge you more</span>. Finally, Wordpress is the no.1 target for hackers. In your first week after lauch your website admin panel will receive hundreds of login attempts from strangers as well as comments from scammers asking you to click on dodgy website links, <span className='font-bold'>costing you time</span> to protect against.</p>
                         </div>
                     </div>
                 </section>
