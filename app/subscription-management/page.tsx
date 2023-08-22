d1903d8daac2e8a5bd3b7cddcd87ff634bc99770
skowalskidev <skowalskidev@gmail.com>
2023-08-22 11:25:20 +0700

    add homepage links

--- app/subscription-management/page.tsx
+++ app/subscription-management/page.tsx
@@ -75,9 +75,9 @@ export default function Home() {
                     <div className="items-center justify-center w-fit md:flex md:order-1 lg:absolute lg:mx-auto lg:left-0 lg:right-0" id="navbar-sticky">
                         <ul className="grow px-5 py-2.5 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-opacity-0">
                             <li>
-                                <a href="#" className="flex items-top text-sm font-light text-gray-500/100 dark:text-gray-400/100 tracking-wider">
+                                <Link href="/" className="flex items-top text-sm font-light text-gray-500/100 dark:text-gray-400/100 tracking-wider">
                                     <p className="text-sm tracking-widest font-light text-gray-500/100 text-gray-900 dark:text-white">Reverie Zero</p>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
@@ -106,7 +106,7 @@ export default function Home() {
             <footer className="bg-white rounded-lg dark:bg-gray-900 m-4">
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <a href="#" className="hover:underline">Reverie Zero</a></span>
+                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <Link href="/" className="hover:underline">Reverie Zero</Link></span>
                 </div>
             </footer>
         </>
--- app/web-development/page.tsx
+++ app/web-development/page.tsx
@@ -7,6 +7,7 @@ import StickyMenuCTA from '../components/StickyMenuCTA';
 import Image from 'next/image';
 import { Dialog, Transition } from '@headlessui/react'
 import { useForm, ValidationError } from '@formspree/react';
+import Link from 'next/link';
 
 function ContactForm() {
     const [state, handleSubmit] = useForm("xoqojrzb");
@@ -157,9 +158,9 @@ export default function Home() {
                     <div className="items-center justify-center w-fit md:flex md:order-1 lg:absolute lg:mx-auto lg:left-0 lg:right-0" id="navbar-sticky">
                         <ul className="grow px-5 py-2.5 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-opacity-0">
                             <li>
-                                <a href="#" className="flex items-top text-sm font-light text-gray-500/100 dark:text-gray-400/100 tracking-wider">
+                                <Link href="/" className="flex items-top text-sm font-light text-gray-500/100 dark:text-gray-400/100 tracking-wider">
                                     <p className="text-sm tracking-widest font-light text-gray-500/100 text-gray-900 dark:text-white">Reverie Zero</p>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
@@ -726,7 +727,7 @@ export default function Home() {
                         </ul>
                     </div>
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <a href="#" className="hover:underline">Reverie Zero</a></span>
+                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <Link href="/" className="hover:underline">Reverie Zero</Link></span>
                 </div>
             </footer>
 
