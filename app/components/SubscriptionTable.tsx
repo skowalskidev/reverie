3351208765367665520fe75b50f693cdc27e4005
skowalskidev <skowalskidev@gmail.com>
2023-09-04 15:12:59 +0900

    fix add new row

--- app/components/SubscriptionTable.tsx
+++ app/components/SubscriptionTable.tsx
@@ -38,32 +38,37 @@ const initialSubscriptions: Subscription[] = [
 
 const SubscriptionTable: React.FC = () => {
     const [subscriptions, setSubscriptions] = useState<Subscription[]>(() => {
+        // @ts-ignore
         const storedData = localStorage.getItem('subscriptions');
         return storedData ? JSON.parse(storedData) : initialSubscriptions;
     });
     const [editableRowIndex, setEditableRowIndex] = useState<number | null>(null);
     const [subscriptionDraftRow, setSubscriptionDraftRow] = useState<Subscription | null>(null);
+    const [prevSubscriptionsLength, setPrevSubscriptionsLength] = useState(subscriptions.length);
 
     useEffect(() => {
-        localStorage.setItem('subscriptions', JSON.stringify(subscriptions));
+        if (subscriptions.length > prevSubscriptionsLength) {
+            startEditRow(subscriptions.length - 1);
+        }
+        setPrevSubscriptionsLength(subscriptions.length);
     }, [subscriptions]);
 
-    const handleEdit = (index: number, key: keyof Subscription, value: any) => {
+    function handleEdit(index: number, key: keyof Subscription, value: any) {
         const newSubscriptions: any[] = [...subscriptions];
         newSubscriptions[index][key] = value;
         setSubscriptions(newSubscriptions);
     };
 
-    const handleAddRow = () => {
+    function handleAddRow() {
         setSubscriptions([...subscriptions, {
             invoiceLink: '',
             name: '',
-            price: '$0',
+            price: '$',
             usedThisMonth: true
         }]);
     };
 
-    const handleDeleteRow = (index: number) => {
+    function handleDeleteRow(index: number) {
         const newSubscriptions = [...subscriptions];
         newSubscriptions.splice(index, 1);
         setSubscriptions(newSubscriptions);
@@ -73,7 +78,7 @@ const SubscriptionTable: React.FC = () => {
     function startEditRow(index: number) {
         setEditableRowIndex(index);
         setSubscriptionDraftRow({ ...subscriptions[index] });
-    }
+    };
 
     function saveEditRow() {
         if (subscriptionDraftRow) {
@@ -82,7 +87,7 @@ const SubscriptionTable: React.FC = () => {
             setSubscriptions(newSubscriptions);
             setEditableRowIndex(null);
         }
-    }
+    };
 
     return (
         <div className="relative overflow-x-auto border-black dark:border-purple-600 border sm:rounded-lg grow">
@@ -98,36 +103,49 @@ const SubscriptionTable: React.FC = () => {
                 </thead>
                 <tbody>
                     {subscriptions.map((sub, index) => (
-                        <tr key={index} className="hover:cursor-pointer hover:text-purple-600 bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-900">
-                            {Object.keys(sub).filter((key) => !(key === 'invoiceLink' && editableRowIndex === null)).map((key, colIndex) => (
-                                <td key={key} className="px-6 py-4" onClick={() => { if (key !== 'usedThisMonth' && editableRowIndex === null) window.open(sub.invoiceLink, '_blank') }}>
-                                    {key === 'usedThisMonth' ? (
-                                        <label className="relative inline-flex items-center cursor-pointer">
-                                            <input type="checkbox" checked={editableRowIndex === index ? subscriptionDraftRow?.usedThisMonth : sub.usedThisMonth} className="sr-only peer" onChange={(e) => editableRowIndex === index ? setSubscriptionDraftRow({
-                                                ...subscriptionDraftRow,
-                                                usedThisMonth: e.target.checked,
-                                            } as Subscription) : handleEdit(index, key, e.target.checked)} />
-                                            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
-                                        </label>
-                                    ) : (
-                                        editableRowIndex === null || editableRowIndex !== index
-                                            ? key === 'invoiceLink' && editableRowIndex === index ? <div></div> : <div className='truncate max-w-xs'>{sub[key as keyof Subscription].toString()}</div>
-                                            : <input
-                                                className={
-                                                    `w-full bg-inherit border-transparent font-extrabold text-lg !bg-gray-50 border !border-gray-300 text-gray-900 rounded-lg focus:ring-purple-text-purple-600 focus:border-purple-text-purple-600 block p-2 px-3 dark:!bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-text-purple-600 dark:focus:border-purple-text-purple-600'
-                                                ${!sub.usedThisMonth && 'text-red-600'}`
-                                                }
-                                                type="text"
-                                                value={subscriptionDraftRow ? subscriptionDraftRow[key as keyof Subscription].toString() : ''}
-                                                onChange={(e) => setSubscriptionDraftRow({
-                                                    ...subscriptionDraftRow,
-                                                    [key as keyof Subscription]: e.target.value,
-                                                } as Subscription)
-                                                }
-                                            />
-                                    )}
-                                </td>
-                            ))}
+                        <tr
+                            key={index}
+                            className="hover:cursor-pointer hover:text-purple-600 bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-900"
+                        >
+                            {Object.keys(sub)
+                                .filter((key) => !(key === 'invoiceLink' && editableRowIndex === null))
+                                .map((key, colIndex) => (
+                                    <td
+                                        key={key}
+                                        className="px-6 py-4"
+                                        onClick={() => { if (key !== 'usedThisMonth' && editableRowIndex === null) window.open(sub.invoiceLink, '_blank') }}
+                                    >
+                                        {key === 'usedThisMonth' ? (
+                                            <label className="relative inline-flex items-center cursor-pointer">
+                                                <input
+                                                    type="checkbox"
+                                                    checked={editableRowIndex === index && subscriptionDraftRow ? subscriptionDraftRow.usedThisMonth : sub.usedThisMonth} className="sr-only peer"
+                                                    onChange={(e) => editableRowIndex === index ? setSubscriptionDraftRow({
+                                                        ...subscriptionDraftRow,
+                                                        usedThisMonth: e.target.checked,
+                                                    } as Subscription) : handleEdit(index, key, e.target.checked)}
+                                                />
+                                                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
+                                            </label>
+                                        ) : (
+                                            editableRowIndex === null || editableRowIndex !== index
+                                                ? key === 'invoiceLink' && editableRowIndex === index ? <div></div> : <div className='truncate max-w-xs'>{sub[key as keyof Subscription].toString()}</div>
+                                                : <input
+                                                    className={
+                                                        `w-full bg-inherit border-transparent font-extrabold text-lg !bg-gray-50 border !border-gray-300 text-gray-900 rounded-lg focus:ring-purple-text-purple-600 focus:border-purple-text-purple-600 block p-2 px-3 dark:!bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-text-purple-600 dark:focus:border-purple-text-purple-600'
+                                                        ${!sub.usedThisMonth && 'text-red-600'}`}
+                                                    type="text"
+                                                    value={subscriptionDraftRow && subscriptionDraftRow[key as keyof Subscription]
+                                                        ? subscriptionDraftRow[key as keyof Subscription].toString()
+                                                        : ''}
+                                                    onChange={(e) => setSubscriptionDraftRow({
+                                                        ...subscriptionDraftRow,
+                                                        [key as keyof Subscription]: e.target.value,
+                                                    } as Subscription)}
+                                                />
+                                        )}
+                                    </td>
+                                ))}
                             <td className="flex gap-4 px-6 py-4 text-right font-extrabold text-lg" onClick={(e) => e.stopPropagation()}>
                                 {editableRowIndex !== index && <>
                                     <span className="cursor-pointer text-gray-400 hover:text-purple-600" onClick={() => startEditRow(index)}>
