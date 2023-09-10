8de9c69c1bcf3108565f81a16a0373bc1819f0d5
skowalskidev <skowalskidev@gmail.com>
2023-09-10 13:26:46 +0900

    add max-w to main

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                 </div>
             </nav>
 
-            <main>
+            <main className='mx-auto max-w-screen-lg'>
                 <section className="bg-white dark:bg-gray-900 mt-28 text-center p-6">
                     <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">free <span className="text-purple-600 dark:text-purple-600">fun</span> easy</h1>
                     <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">A collection of tools made by myself for myself with the hopes of helping others</p>
