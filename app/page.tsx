e7dbf84fad2e3b343f66d90f3cc60188efb02762
skowalskidev <skowalskidev@gmail.com>
2023-09-11 20:20:26 +0900

    remove my email

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
                 <section className="bg-white dark:bg-gray-900 mt-28 text-center p-6">
                     <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">free <span className="text-purple-600 dark:text-purple-600">fun</span> easy</h1>
                     <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">A collection of tools made by myself for myself with the hopes of helping others</p>
-                    <p><a href="mailto:skowalskidev@gmail.com" className='text-lg text-purple-600'><span className='text-gray-500 dark:text-gray-400'>Suggestions or queries? Email me</span> skowalskidev@gmail.com</a></p>
+                    {/* <p><a href="mailto:skowalskidev@gmail.com" className='text-lg text-purple-600'><span className='text-gray-500 dark:text-gray-400'>Suggestions or queries? Email me</span> skowalskidev@gmail.com</a></p> */}
                 </section>
 
                 <section className="grid grid-cols-2 md:grid-cols-5 gap-4 p-6">
