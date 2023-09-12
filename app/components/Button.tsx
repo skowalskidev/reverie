c454b88f0c418ee03e970e9cc612a1cf24515021
skowalskidev <skowalskidev@gmail.com>
2023-09-12 13:51:46 +0900

    button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+type Props = {
+    onClick?: () => void;
+    children: React.ReactNode;
+}
+
+const Button = ({ onClick, children }: Props) => {
+    return (
+        <button type="button" onClick={onClick} className="grow inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-purple-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <div className='grow px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+                {children}
+            </div>
+        </button>
+    );
+};
+
+export default Button;
