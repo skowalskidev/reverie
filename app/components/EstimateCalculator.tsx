ec8323f88662aa4fee4874a605f7001fc571190f
skowalskidev <skowalskidev@gmail.com>
2023-09-14 10:38:04 +0900

    legacy next image conversion

--- app/components/EstimateCalculator.tsx
+++ app/components/EstimateCalculator.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 const EstimateCalculator: React.FC = () => {
     const [numberOfPages, setNumberOfPages] = useState(1);
--- app/web-development/page.tsx
+++ app/web-development/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ExampleTable from '../components/ExampleTable';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import ScaleMain from '../components/ScaleMain';
 import Navigation from '../components/Navigation';
 import Link from 'next/link';
