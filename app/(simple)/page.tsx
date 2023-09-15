3248235e0fc44fd520d2510cc5ce9dc4642ab200
skowalskidev <skowalskidev@gmail.com>
2023-09-15 13:26:31 +0900

    fix blog card colors

--- app/(simple)/page.tsx
+++ app/(simple)/page.tsx
@@ -71,7 +71,7 @@ export default async function Home() {
                                                 {author}
                                             </span>
                                         </div>
-                                        <div className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                                        <div className="inline-flex items-center font-medium text-purple-600 dark:text-white hover:underline">
                                             Read more
                                             <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                         </div>
