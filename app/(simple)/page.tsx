0fc29e4298cbb5298122a7342b53b1713c0a19e1
skowalskidev <skowalskidev@gmail.com>
2023-09-16 22:46:46 +0900

    update blog link style

--- app/(simple)/page.tsx
+++ app/(simple)/page.tsx
@@ -49,16 +49,15 @@ export default async function Home() {
                     <div className="grid gap-8 lg:grid-cols-2">
                         {allPostsData.map(({ id, date, title, author, authorImage, preview }, index) => (
                             <Link key={index} href={`/posts/${id}`}>
-                                <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                    <div className="flex justify-between items-center mb-5 text-gray-500">
+                                <article className="outline outline-black text-black hover:!text-purple-600 hover:!outline-purple-600 dark:outline-white dark:text-white p-3 rounded flex flex-col justify-between gap-8 relative">
+                                    <div className="flex justify-between items-center text-gray-500">
                                         <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                                             <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
                                             Article
                                         </span>
                                         <span className="text-sm"><Date dateString={date} /></span>
                                     </div>
-                                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h2>
-                                    <div className="line-clamp-3 mb-5 font-light text-gray-500 dark:text-gray-400" dangerouslySetInnerHTML={{ __html: preview ? preview.toString() : '' }}></div>
+                                    <h2 className="text-2xl font-bold tracking-tight ">{title}</h2>
                                     <div className="flex justify-between items-center">
                                         <div className="flex items-center space-x-4">
                                             <Image className="w-7 h-7 rounded-full object-cover"
@@ -67,11 +66,11 @@ export default async function Home() {
                                                 src={authorImage}
                                                 alt="Jese Leos avatar"
                                             />
-                                            <span className="font-medium dark:text-white">
+                                            <span className="font-medium">
                                                 {author}
                                             </span>
                                         </div>
-                                        <div className="inline-flex items-center font-medium text-purple-600 dark:text-white hover:underline">
+                                        <div className="inline-flex items-center font-medium hover:underline">
                                             Read more
                                             <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                         </div>
