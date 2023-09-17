29dd97ea1c3193f8b70ae9496452cdc9a064bf2b
skowalskidev <skowalskidev@gmail.com>
2023-09-17 11:33:27 +0900

    fix map width

--- blog/post1.tsx
+++ blog/post1.tsx
@@ -13,8 +13,7 @@ export default async function Post() {
         <>
             <div className="flex flex-col gap-6 justify-between px-4 mx-auto max-w-screen-xl text-gray-900 dark:text-gray-400" style={{ gap: '24px' }}>
                 <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert flex flex-col gap-4">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.419645034808!2d100.47663647509248!3d13.873838086530869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ad4b986bed7%3A0xc407310b6ec1aef8!2sThe%20Politan%20Rive!5e0!3m2!1sen!2sus!4v1694872659853!5m2!1sen!2sus" width="600" height="450" className="border-none" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3873.419645034808!2d100.47663647509248!3d13.873838086530869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ad4b986bed7%3A0xc407310b6ec1aef8!2sThe%20Politan%20Rive!5e0!3m2!1sen!2sus!4v1694872659853!5m2!1sen!2sus" height="450" className="border-none" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     <p>I have lived it, here's how.</p>
 
                     <p>The place is called <b>The Politan Rive</b> and it's located in a city near Bangkok, Thailand called Nonthaburi.</p>
