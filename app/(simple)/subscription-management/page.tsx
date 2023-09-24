b5caf024828d165a08f7037d902dca1a7cedae7a
skowalskidev <skowalskidev@gmail.com>
2023-09-24 22:30:03 +0900

    add alpha to subscription management

--- app/(simple)/subscription-management/page.tsx
+++ app/(simple)/subscription-management/page.tsx
@@ -11,8 +11,9 @@ export const metadata = {
 export default function Home() {
     return (
         <>
-            <section className="bg-white dark:bg-gray-900 text-center p-6">
-                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tigh md:text-5xl lg:text-6xl dark:text-white">Subscription Management</h1>
+            <section className="flex flex-col gap-4 items-center bg-white dark:bg-gray-900 text-center p-6">
+                <span className="lg:hidden bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-pink-400 border border-pink-400">Alpha</span>
+                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tigh md:text-5xl lg:text-6xl dark:text-white relative">Subscription Management<span className="hidden lg:block absolute top-0 -right-20 bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-pink-400 border border-pink-400">Alpha</span></h1>
                 <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Keep track of all subscriptions and invoices for expense accounting</p>
             </section>
             <section className="bg-white dark:bg-gray-900 text-center p-6 flex flex-col items-center">
