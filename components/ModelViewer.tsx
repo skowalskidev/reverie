e1a60b148abd19d1cc0ec7b404c749eeb39feba5
skowalskidev <skowalskidev@gmail.com>
2023-10-18 12:20:11 +0900

    add orbitControls and basic loader

--- components/ModelViewer.tsx
+++ components/ModelViewer.tsx
@@ -1,12 +1,15 @@
 'use client'
-// components/ModelViewer.tsx
-import { useEffect, useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const ModelViewer = () => {
     const containerRef = useRef<HTMLDivElement | null>(null);
     const modelRef = useRef<THREE.Group | null>(null);  // Create a ref to hold your model
+    const [autoRotate, setAutoRotate] = useState(true);
+    const controlsRef = useRef<OrbitControls | null>(null);
+    const [loading, setLoading] = useState(true);  // Add this line to manage loading state
 
     useEffect(() => {
         const scene = new THREE.Scene();
@@ -37,31 +40,48 @@ const ModelViewer = () => {
             (gltf) => {
                 modelRef.current = gltf.scene;  // Store the model in the ref
                 scene.add(gltf.scene);
-                camera.position.set(3, 2, 0);
-                camera.lookAt(0, 1.3, 0);
-                const animate = () => {
-                    requestAnimationFrame(animate);
-                    if (modelRef.current) {
-                        modelRef.current.rotation.y += 0.01;  // Rotate the model
-                    }
-                    renderer.render(scene, camera);
-                };
-                animate();
+                camera.position.set(3, 2, 2);
+                setLoading(false);  // Set loading to false once the model is loaded
             },
-            undefined,
-            (error) => console.error(error)
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');  // Optional: log loading progress
+            },
+            (error) => {
+                console.error(error);
+                setLoading(false);  // Optionally, set loading to false also on error
+            }
         );
 
-        return () => {
-            if (containerRef.current) {
-                containerRef.current.removeChild(renderer.domElement);
+        controlsRef.current = new OrbitControls(camera, renderer.domElement);
+        controlsRef.current.addEventListener('start', () => setAutoRotate(false));
+        controlsRef.current.addEventListener('end', () => setAutoRotate(true));
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            if (modelRef.current && autoRotate) {
+                modelRef.current.rotation.y += 0.01;  // Rotate the model
             }
+            if (controlsRef.current) {
+                controlsRef.current.update();  // Only call update in the animate function
+            }
+            camera.lookAt(0, 1.3, 0);  // Update camera lookAt in the animate function
+            renderer.render(scene, camera);
+        };
+        animate();
+
+        return () => {
+            containerRef.current?.removeChild(renderer.domElement);
             // scene.dispose();
             renderer.dispose();
+            controlsRef.current?.dispose();
         };
-    }, []);
+    }, []);  // Empty dependency array ensures this useEffect runs once on mount
 
-    return <div className='h-48' ref={containerRef} />;
+    return (
+        <div className='h-48 flex items-center justify-center' ref={containerRef}>
+            {loading && <div className="loader">Loading the lucky cat...</div>}  {/* Display loader when loading */}
+        </div>
+    );
 };
 
 export default ModelViewer;
