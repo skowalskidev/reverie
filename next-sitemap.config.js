2c88f0783aa36fc72829185de9a595856bcc02f4
skowalskidev <skowalskidev@gmail.com>
2023-10-27 14:55:30 +0900

    add blog to the sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,20 @@
+// next-sitemap.config.js
+
+const fs = require('fs');
+const path = require('path');
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'siteUrlNotConfigured.com',
     generateRobotsTxt: true,
-}
+    additionalPaths: async () => {
+        const filePath = path.join(process.cwd(), 'blog');
+        const files = fs.readdirSync(filePath);
+        const blogPostPaths = files.map(file => ({
+            loc: `/blog/${path.basename(file, path.extname(file))}`,
+            changefreq: 'daily',
+            priority: 0.7,
+        }));
+        return blogPostPaths;
+    },
+};
