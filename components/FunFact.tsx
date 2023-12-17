d68247e861581db71cd5f13822da88b42654f884
skowalskidev <skowalskidev@gmail.com>
2023-12-17 17:04:37 +0700

    improve fun facts

--- components/FunFact.tsx
+++ components/FunFact.tsx
@@ -4,21 +4,21 @@ import { useEffect, useState } from "react";
 import Skeleton from "./Skeleton";
 
 const funFacts = [
-    'Internet\'s birth: 1969, ARPANET sparked the digital revolution.',
-    '40% global population online - a virtual world for billions!',
-    '1.88 billion websites - a vast cyber universe to explore.',
-    'Emoji: modern language transcending words and borders.',
-    'Google\'s origin: "Backrub" search engine, an intriguing start.',
-    'Cat videos rule - feline fascination knows no bounds!',
-    'Internet addiction real - the digital siren\'s call.',
-    '3.5 billion daily searches - endless quest for knowledge.',
-    '1st webcam captured a coffee pot\'s daily drama.',
-    'YouTube, 1B+ users - a stage for creativity worldwide.',
-    'Amazon\'s humble beginning as an online bookstore.',
-    '90 trillion emails/year - a digital tidal wave.',
-    'World\'s largest library - knowledge at your fingertips.',
-    '50+ million tweets/day - the world\'s constant conversation.',
-    'Internet of Things (IoT) connects everyday objects to the web.',
+    'Google processes over 3.5 billion searches daily',
+    'Facebook has 2.8 billion users across all continents',
+    '5 billion YouTube videos watched daily',
+    'Amazon ships 1.6 million packages per day',
+    'Twitter sees 500 million tweets sent daily',
+    'Instagram hosts 95 million photo uploads daily',
+    '306 billion emails sent globally every day',
+    'TikTok has 1 billion active users',
+    'LinkedIn connects 810 million professionals',
+    'Snapchat sees 5 billion snaps created daily',
+    'Netflix streams 1 billion hours weekly',
+    'Spotify offers over 70 million songs',
+    'Apple App Store features 2.2 million apps',
+    'WhatsApp sees 100 billion messages sent daily',
+    'Tesla has 1 million electric cars on the roads',
 ]
 
 const getFunFact = () => {
